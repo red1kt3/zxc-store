9b45f07c20d59d6fcdb94dd2842ca625556df098
red1kt3 <143636464+red1kt3@users.noreply.github.com>
2023-12-09 13:50:01 +0200

    blog_views

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from apps.blog.models import BlogCategory
+from apps.blog.models import BlogCategory, Article, Tag
 
 
 def blog_category_list(request):
     blog_categories = BlogCategory.objects.all()
     return render(request, 'blog/category/list.html', {'categories': blog_categories})
+
+
+def article_list(request, category_id):
+    articles = Article.objects.filter(category=category_id)
+    return render(request, 'blog/article/list.html', {'articles': articles})
+
+
+def article_view(request, category_id, article_id):
+    article = Article.objects.get(id=article_id)
+    category = BlogCategory.objects.get(id=category_id)
+    return render(request, 'blog/article/view.html', {'article': article, 'category': category})
+
+
+def tags_to_articles(request, tag_id):
+    articles = Article.objects.filter(tags__id=tag_id)
+    tag = Tag.objects.get(id=tag_id)
+    return render(request, 'blog/tag/list.html', {'articles': articles, 'tag': tag})
 
 
