4682a024a7b145a5fed95386cf67fa3350c0ca00
red1kt3 <143636464+red1kt3@users.noreply.github.com>
2023-12-10 14:43:36 +0200

    admin_view

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 from imagekit.models import ProcessedImageField, ImageSpecField
 from pilkit.processors import ResizeToFill
+from config.settings import MEDIA_ROOT
 
 class BlogCategory(models.Model):
     name = models.CharField(verbose_name='Название', max_length=255)
@@ -13,6 +15,23 @@ class BlogCategory(models.Model):
         options={'quality':100},
         null=True,
     )
+
+
+    def image_tag_thumbnail(self):
+        if self.image:
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image}' width='100'>")
+
+    image_tag_thumbnail.short_description = 'Текущее изображение'
+    image_tag_thumbnail.allow_tags = True
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image}' width='400'>")
+
+    image_tag.short_description = 'Текущее изображение'
+    image_tag.allow_tags = True
+
+
     def __str__(self):
         return self.name
 
@@ -49,6 +68,23 @@ class Article(models.Model):
     updated_at = models.DateTimeField(verbose_name='Время обновления', auto_now=True)
     tags = models.ManyToManyField(to=Tag, verbose_name='Тэг', blank=True)
 
+    def image_tag_thumbnail(self):
+        if self.image:
+            if not self.image_thumbnail:
+                Article.objects.get(id=self.id)
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}' width='100'>")
+
+    image_tag_thumbnail.short_description = 'Текущее изображение'
+    image_tag_thumbnail.allow_tags = True
+
+    def image_tag(self):
+        if self.image:
+            if not self.image_thumbnail:
+                Article.objects.get(id=self.id)
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}' width='400'>")
+
+    image_tag.short_description = 'Текущее изображение'
+    image_tag.allow_tags = True
 
     image = ProcessedImageField(
         verbose_name='Изображение',
