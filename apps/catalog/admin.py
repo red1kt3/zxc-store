e76008e59cc1ef3b32c49431e7b040cdbb824759
red1kt3 <143636464+red1kt3@users.noreply.github.com>
2023-12-24 14:53:48 +0200

    product images

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -7,9 +7,13 @@ class CategoryADMIN(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
 
 
+class ProductCategoryInLine(admin.TabularInline):
+    model = Product.categories.through
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'quantity']
+    list_display = ['id', 'name', 'price', 'quantity',]
+    inlines = [ProductCategoryInLine]
     list_display_links = ['id', 'name', 'price', 'quantity']
 
