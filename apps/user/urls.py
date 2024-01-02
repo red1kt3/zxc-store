2fafcd186dd71982021b8b5982ae7b97af9dc902
red1kt3 <143636464+red1kt3@users.noreply.github.com>
2024-01-02 17:18:18 +0200

    meta tags

---
+++ apps/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.user import views
+
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout')
+]
