9fd6e96b934ee2d879b88853121f3da217fcea7d
red1kt3 <143636464+red1kt3@users.noreply.github.com>
2024-01-03 17:26:42 +0200

    meta tags

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,8 +1,28 @@
 from django import forms
 
+from apps.user.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.ModelForm):
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'image', 'first_name', 'last_name', 'email', 'phone', 'password']
+
+
+
+
+    def clean_password_confirm(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['password'] != cleaned_data['password_confirm']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cleaned_data['password_confirm']
+
 
 
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,5 +4,6 @@ from apps.user import views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout')
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register')
 ]
